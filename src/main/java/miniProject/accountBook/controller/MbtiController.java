5696bbf00cdd8188e6e2673dfd90a81f43d37729
0Chord <seaweed.0chord@gmail.com>
2022-11-04 22:26:43 +0900

    mbti 검사 화면 폼 추가

--- src/main/java/miniProject/accountBook/controller/MbtiController.java
+++ src/main/java/miniProject/accountBook/controller/MbtiController.java
@@ -1,4 +1,38 @@
 package miniProject.accountBook.controller;
 
+import miniProject.accountBook.domain.Member;
+import miniProject.accountBook.dto.MbtiForm;
+import miniProject.accountBook.service.MbtiService;
+import miniProject.accountBook.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mbti")
 public class MbtiController {
+
+    MbtiService mbtiService;
+    MemberService memberService;
+
+    public MbtiController(MbtiService mbtiService, MemberService memberService) {
+        this.mbtiService = mbtiService;
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/{id}/mbtiForm")
+    public String viewMbti(Model model, @PathVariable("id") String id){
+        Member member = memberService.findOne(id);
+        model.addAttribute("member",member);
+        return "mbti/mbtiForm";
+    }
+
+    @PostMapping("/{id}/registration")
+    public String analyze(@Validated MbtiForm mbtiForm, Model model, @PathVariable("id") String id){
+        return null;
+    }
 }
--- src/main/java/miniProject/accountBook/dto/MbtiForm.java
+++ src/main/java/miniProject/accountBook/dto/MbtiForm.java
@@ -5,5 +5,15 @@ import lombok.Data;
 @Data
 public class MbtiForm {
     private String username;
-    private Integer[] scores;
+    private Long firstProblem;
+    private Long secondProblem;
+    private Long thirdProblem;
+    private Long fourthProblem;
+    private Long fifthProblem;
+    private Long sixthProblem;
+    private Long seventhProblem;
+    private Long eighthProblem;
+    private Long ninthProblem;
+    private Long tenthProblem;
+    private Long score;
 }
